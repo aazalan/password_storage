2f5c20af9e0fb6c9e59f3658f046b298fabd7a2d
Alina Abdul <alinka140901@gmail.com>
2023-10-30 17:32:40 +0300

    Удален файл старого проекта

--- routes/web.php
+++ routes/web.php
@@ -15,66 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-// Route::get('login', [LoginController::class, 'create'])->name('login');
-
-// Route::post('login', [LoginController::class, 'store'])->name('login');
-
-// Route::post('logout', [LoginController::class, 'destroy'])->middleware('auth');
-
-
-// Route::middleware('auth')->group(function() {
-
-//     Route::get('/', function () {
-//         return Inertia::render('Home');
-//     });
-    
-    
-//     Route::get('/users', function (Request $request) {
-//         return Inertia::render('Users/Index', [
-//             'users' => User::query()
-//                 ->when($request->search, function ($query, $search) {
-//                     $query->where('name', 'like', '%' . $search . '%');
-//                 })
-//                 ->paginate(10)
-//                 ->withQueryString()
-//                 ->through(fn($user) => [
-//                     'id' => $user->id,
-//                     'name' => $user->name,
-//                     'can' => [
-//                         'editUser' => Auth::user()->can('edit', $user)
-//                     ]
-//                 ]),
-//             'filters' => $request->only('search'),
-//             'can' => [
-//                 'createUser' => Auth::user()->can('create', User::class)
-//             ]
-//         ]);
-//     });
-    
-    
-//     Route::get('/users/create', function () {
-//         return Inertia::render('Users/Create');
-//     })->can('create', 'App\Models\User');
-    
-    
-//     Route::post('/users', function (Request $request) {
-//         $validated = $request->validate([
-//             'name' => 'required',
-//             'email' => ['required', 'email', 'unique:users,email'],
-//             'password' => 'required',
-//         ]);
-    
-//         User::create($validated);
-        
-//         return redirect('/users');
-//     });
-    
-    
-//     Route::get('/settings', function () {
-//         return Inertia::render('Settings');
-//     });
-
-// });
 
 Route::get('/', [PasswordController::class, 'index']);
 
